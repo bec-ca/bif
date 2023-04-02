9bc2026cd816c112a061753e3776a8ea3c84cd6b
Rebecca Ribas <beky.ribas@gmail.com>
2023-04-01 22:19:46 -0400

    missing package

--- ci.sh
+++ ci.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -eu
 
 ./download-pkg.sh bee https://github.com/bec-ca/bee/archive/refs/tags/v1.1.0.tar.gz
+./download-pkg.sh bee https://github.com/bec-ca/command/archive/refs/tags/v1.0.0.tar.gz
 
 for file in bif/*.cpp; do
   echo "Compiling $file..."
